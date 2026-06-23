87cb568437ca256b2f92535fd1c549bd3107d69d
Reut Gershon <48221955+reutg@users.noreply.github.com>
2026-06-23 18:05:13 +0300

    remove unused import

--- src/features/boxes/index.ts
+++ src/features/boxes/index.ts
@@ -1,4 +1,3 @@
-export { default as MyBoxes } from "./components/my-boxes";
 export { useAddBoxForm } from "./hooks/use-add-box-form";
 export { useBoxesList } from "./hooks/use-boxes-list";
 export { type BoxFormValues, BoxFormValuesSchema } from "./schemas/box-form-schema";
